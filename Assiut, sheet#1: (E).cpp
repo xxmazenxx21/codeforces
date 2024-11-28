b5ec14528b34ee8f620b05d280ab6ead8ef8f6b3
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-11-28 10:55:49 +0200

    Create Assiut, sheet#1: (E).cpp

---
+++ Assiut, sheet#1: (E).cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+
+
+int main() {
+cout << fixed << setprecision(9);
+	double  R; 
+	cin >> R; 
+	double BI = 3.141592653;
+	 
+	cout << (R * R) * BI;
+
+}
