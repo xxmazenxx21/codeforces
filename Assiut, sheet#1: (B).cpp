c97a3f4c6fdc455a5e08a8859c59a19abe57e3f2
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-11-28 10:20:02 +0200

    Create Assiut, sheet#1: (B).cpp

---
+++ Assiut, sheet#1: (B).cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int i; 
+	float f; 
+	char ch; 
+	double d; 
+	long long l; 
+	cin >> i >> l >> ch >> f >> d;
+
+	cout << i << "\n" << l << "\n" << ch << "\n" << f << "\n" << d;
+
+
+}
