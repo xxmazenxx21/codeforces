97dcf84c1f0b84ed528ca2307e4c9bd6a08a26f0
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-11-28 10:39:38 +0200

    Create Assiut, sheet#1: (C).cpp

---
+++ Assiut, sheet#1: (C).cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int long long X, Y;
+    cin >> X >> Y;
+
+    cout << X << " + " << Y << " = " << X + Y << "\n";
+    cout << X << " * " << Y << " = " << X * Y << "\n";
+    cout << X << " - " << Y << " = " << X - Y ;
+
+    return 0;
+}
