5c4a09da283bf972591c37ec7de37e5f5552b591
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-11-29 10:46:00 +0200

    Create Assiut, sheet#1: (G).cpp

---
+++ Assiut, sheet#1: (G).cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+
+
+int main() {
+	// partial sums 
+	long long int N;
+	cin >> N;
+	int long long thenextN = 1+N;
+	int long long summation = (N * thenextN) / 2;
+		cout << summation; 
+	 
+
+	
+		
+
+
+
+	
+
+
+
+}
