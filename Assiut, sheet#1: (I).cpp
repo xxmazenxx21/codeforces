fb7ad030fad8a33d516d9d459ae2a68456841fe2
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-11-29 11:05:08 +0200

    Create Assiut, sheet#1: (I).cpp

---
+++ Assiut, sheet#1: (I).cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+#include <cmath>
+
+
+int main() {
+	 short int  x, y; 
+	cin  >>x >> y ; 
+	if (x >= y) {
+		cout << "Yes";
+	}
+	else {
+		cout << "No";
+	}
+
+}
