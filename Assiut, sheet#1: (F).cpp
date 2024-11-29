3d463be4f0bcb5c65acd1ce0b9e8cd8e25adfab0
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-11-29 10:26:12 +0200

    Create Assiut, sheet#1: (F).cpp

---
+++ Assiut, sheet#1: (F).cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+
+
+int main() {
+
+	int long long N, M, last1, last2; 
+
+	cin >> N >> M; 
+
+	last1 = N % 10; 
+	last2 = M % 10; 
+	cout << last1 + last2; 
+
+
+
+	
+
+
+
+}
