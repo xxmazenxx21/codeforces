338c4505f7194aa073fdbe1ebdc6e755605abbb0
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-11-30 11:37:57 +0200

    Create Assiut, sheet#1: (L).cpp

---
+++ Assiut, sheet#1: (L).cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+#include <cmath>
+
+
+int main() {
+	string f1, s1, f2, s2; 
+	cin >> f1 >> s1; 
+	cin >> f2 >> s2; 
+	if (s1 == s2) {
+		cout << "ARE Brothers";
+
+	}
+	else
+		cout << "NOT"; 
+
+	
+}
