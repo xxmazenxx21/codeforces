c2b6f2ba9d19df99c4da321c28e9026c57bbcf3d
mazen_ahmed21 <114542752+xxmazenxx21@users.noreply.github.com>
2024-12-10 18:48:26 +0200

    Create Assiut, sheet#1: (P).cpp

---
+++ Assiut, sheet#1: (P).cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+#include<iomanip>
+#include <cmath>
+#include <cctype>
+
+
+int main() {
+	
+		
+	int long long n; 
+	cin >> n; 
+	while (n>=10)
+	{
+		n = n / 10; 
+	}
+	if (n % 2 == 0) {
+		cout << "EVEN";
+	}
+	else cout << "ODD";
+
+
+
+
+
+
+	}
+	
